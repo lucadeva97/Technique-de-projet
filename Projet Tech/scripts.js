e9c46b2002692971994af1af9dfdeedf9bf0d867
neveshaiane <122647976+neveshaiane@users.noreply.github.com>
2025-01-24 15:10:19 +0100

    Add files via upload

--- Projet Tech/scripts.js
+++ Projet Tech/scripts.js
@@ -81,6 +81,10 @@ function initMap() {
             }
             infowindow.open(map, marker);
             currentInfoWindow = infowindow;
+
+        // Centrer la carte sur le point trouvé
+        map.setCenter(point.coords);
+        map.setZoom(10);
         });
     });
 }
