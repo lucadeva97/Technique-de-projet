8af10c285484f8e88cdc49bb5b82b18e73d52d91
neveshaiane <122647976+neveshaiane@users.noreply.github.com>
2025-02-03 01:41:01 +0100

    Add files via upload

--- Projet Tech/scripts.js
+++ Projet Tech/scripts.js
@@ -11,10 +11,10 @@ function initMap() {
         { coords: { lat: 48.8566, lng: 2.3522 }, lang: 'fr', text: 'Accent de Paris', img: 'images/paris.jpeg' }, // Paris, France
         { coords: { lat: -27.5954, lng: -48.5480 }, lang: 'pt', text: 'Accent de Florianópolis', img: 'images/flo.jpg', audio: 'extraits/florianopolis.ogg' }, // Florianópolis, Brésil
         { coords: { lat: -3.7172, lng: -38.5434 }, lang: 'pt', text: 'Accent de Fortaleza', img: 'images/forta.jpg', audio: 'extraits/fortaleza.ogg' }, // Fortaleza, Brésil
-        { coords: { lat: 45.5017, lng: -73.5673 }, lang: 'fr', text: 'Accent de Montréal', img: 'images/montreal.jpeg', audio: 'extraits/montreal.mp3' }, // Montréal, Canada
+        { coords: { lat: 45.5017, lng: -73.5673 }, lang: 'fr', text: 'Accent de Montréal', img: 'images/montreal.jpeg', audio: 'extraits/montreal.mp3', href: 'montreal/quebec.html' }, // Montréal, Canada
         { coords: { lat: 40.4168, lng: -3.7038 }, lang: 'es', text: 'Acento de Madrid', img: 'images/madrid.jpg' }, // Madrid, Espagne
         { coords: { lat: 45.1885, lng: 5.7245 }, lang: 'fr', text: 'Accent de Grenoble', img: 'images/grenoble.jpg' }, // Grenoble, France
-        { coords: { lat: 43.2965, lng: 5.3698 }, lang: 'fr', text: 'Accent de Marseille', img: 'images/marseille.jpeg' }, // Marseille, France
+        { coords: { lat: 43.2965, lng: 5.3698 }, lang: 'fr', text: 'Accent de Marseille', img: 'images/marseille.jpeg', href: 'marseille.html' }, // Marseille, France
         { coords: { lat: 45.4408, lng: 12.3155 }, lang: 'it', text: 'Accent de Venise', img: 'images/venise.jpeg' }, // Venise, Italie
         { coords: { lat: -23.5505, lng: -46.6333 }, lang: 'pt', text: 'Accent de São Paulo', img: 'images/sp.jpg', audio: 'extraits/saopaulo.mp4' }, // São Paulo, Brésil
         { coords: { lat: 41.9028, lng: 12.4964 }, lang: 'it', text: 'Accent de Roma', img: 'images/rome.jpeg' }, // Rome, Italie
@@ -45,7 +45,9 @@ function initMap() {
         if (point.audio) {
             contentString += `<br><audio controls><source src="${point.audio}" type="audio/ogg">Your browser does not support the audio element.</audio>`;
         }
-
+        if (point.href) {
+            contentString += `<br><a href="${point.href}" target="_blank">En savoir plus</a>`;
+        }
         const infowindow = new google.maps.InfoWindow({
             content: contentString
         });
