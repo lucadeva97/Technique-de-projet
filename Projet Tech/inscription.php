fed397a90ed3862568b891603c30b040bacae3cf
lucadeva97 <lucadeva97@gmail.com>
2025-03-04 17:00:33 +0100

    Update inscription.php

--- Projet Tech/inscription.php
+++ Projet Tech/inscription.php
@@ -1,6 +1,14 @@
 <?php
 session_start();
 
+
+if (isset($_SESSION['nom']) && isset($_SESSION['email']) && 
+isset($_SESSION['prenom']) && isset($_SESSION['niveau']) && 
+isset($_SESSION['statu'])) {
+    header('Location: compte.php'); 
+    exit();
+} else {
+
 $inscription_reussie = false;
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
@@ -18,7 +26,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $nom = htmlspecialchars($_POST['nom']);
         $prenom = htmlspecialchars($_POST['prenom']);
         $niveau = htmlspecialchars($_POST['niveau']);
-        $statu = htmlspecialchars($_POST['catégorie']); // Ajout de la catégorie
+        $statu = htmlspecialchars($_POST['categorie']); // Ajout de la catégorie
 
         // Vérifier si l'email existe déjà
         $stmt = $pdo->prepare("SELECT * FROM login1 WHERE email = ?");
@@ -52,6 +60,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         die("Erreur lors de l'inscription : " . $e->getMessage());
     }
 }
+}
 ?>
 
 <!DOCTYPE html>
@@ -70,17 +79,17 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 <select id="language-select" onchange="changeLanguage()">
                     <option value="" class="accent">Tu cherches une langue ? </option>
                     <option value="es" disabled style="color: grey;">Espagnol</option>
-                    <option value="fr" data-url="../accentfr.html">Français</option>
+                    <option value="fr" data-url="../accentfr.php">Français</option>
                     <option value="it" disabled style="color: grey;">Italien</option>
                     <option value="pt" disabled style="color: grey;">Portugais</option>
                 </select>
             </div>
         </div>
-        <a href="../index.html"><div class="logo" data-tooltip="homepage"></div></a>
+        <a href="../index.php"><div class="logo" data-tooltip="homepage"></div></a>
         <div></div>
         <div class="right-align">
-            <a href="../apropos.html"><div class="apropos" data-tooltip="à propos"></div></a>
-            <a href="connexion.php"><div class="connexion" data-tooltip="connexion"></div></a>
+            <a href="../apropos.php"><div class="apropos" data-tooltip="à propos"></div></a>
+            <a href="inscription.php"><div class="connexion" data-tooltip="connexion"></div></a>
         </div>
     </div>
 </header>
@@ -104,12 +113,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             <input type="text" id="prenom" name="prenom" required>
 
             <label for="prof">Tu es :</label>
-            <select id="prof" name="catégorie" required>
+            <select id="prof" name="categorie" required>
                 <option value="prof">Professeur de langues</option>
                 <option value="apprenant">Apprenant</option>
             </select>
 
-            <label for="niveau">Si tu es apprenant, précise votre niveau :</label>
+            <label for="niveau">Si tu es apprenant, précise ton niveau :</label>
             <select id="niveau" name="niveau" required>
                 <option value="A1">A1</option>
                 <option value="A2">A2</option>
